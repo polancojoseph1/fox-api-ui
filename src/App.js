95f72be876c01337ca8b0d1fd6021b61ac949057
Joseph <polanco.joseph1@gmail.com>
2022-04-08 10:50:16 -0400

    update Joseph Polanco

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './Nav';
-import Main from './Main';
-import { useEffect, useState } from "react"
-// https://randomfox.ca/floof/ <- random fox api
+import { useEffect, useState } from "react";
+
 function App() {
-  useEffect(() => {
-    localStorage.clear();
-  }, [])
+
+  let [modalUrl, setModalUrl] = useState("");
+
+  function Nav() {
+    return (<div className="nav"></div>)
+  }
+
+  function Main() {
+    let rows = new Array(6).fill(<Row />);
+    return (<div className="main">
+      { rows }
+    </div>)
+  }
+  
+  function Row(props) {
+    let [set, setSet] = useState(new Set());
+  
+    let images = new Array(3).fill(<Image set={ set }/>)
+    
+    return (<div className="row">
+      {images}
+    </div>)
+  }
+
+  function Image(props) {
+    let [url, setUrl] = useState("");
+
+    useEffect(() => {
+      fetch("https://randomfox.ca/floof/")
+        .then(res => res.json())
+      .then(data => setUrl(data ? data.image : null))
+    }, [])
+
+    const setModalImage = () => {
+      setModalUrl(url);
+    }
+  
+    return (<div onClick={setModalImage} className="image">
+      <img src={ url } width="200" height="200"/>
+    </div>)
+  }
+
+  function ModalContainer(props) {
+    let url = modalUrl;
+    let display = url ? "" : "none";
+    const handleClick = () => {
+      setModalUrl("");
+    }
+    return (
+      <div onClick={handleClick} className="modal-container" url={url} style={{display: display}}>
+        <Modal/>
+      </div>
+    )
+  }
+
+  function Modal(props) {
+    return (
+      <div className="modal">
+        <img className="modal-image" src={ modalUrl }/>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <Nav />
       <Main />
+      <ModalContainer/>
     </div>
   );
 }
--- src/Main.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-import Row from "./Row"
-export default function Main() {
-  let rows = new Array(6).fill(<Row />);
-  return (<div className="main">
-    { rows }
-  </div>)
-}
